608391e3755081131304802fa389b481a3b97ac8
Sergey Lugovoy <serg.lugovoy.81@gmail.com>
2020-12-11 15:14:08 -0700

    modified selectAll function

--- config/orm.js
+++ config/orm.js
@@ -50,7 +50,7 @@ const orm = {
       cb(data);
     });
   },
-  create: function (table, cols, vals, cb) {
+  insertOne: function (table, cols, vals, cb) {
     var queryString = "INSERT INTO " + table;
 
     queryString += " (";
@@ -71,7 +71,7 @@ const orm = {
     });
   },
   // An example of objColVals would be {name: panther, sleepy: true}
-  update: function (table, objColVals, condition, cb) {
+  updateOne: function (table, objColVals, condition, cb) {
     var queryString = "UPDATE " + table;
 
     queryString += " SET ";
